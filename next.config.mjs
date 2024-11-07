4f36751c63fbb793fb1a9ea42af332fd10f68212
academy17 <112261995+academy17@users.noreply.github.com>
2024-11-07 00:03:59 +0000

    Add ignoreBuildErrors to Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  };
+  
+  export default nextConfig;
+  
